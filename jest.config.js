a80b7c86b97b52766dbb3ef5ea3f77f62eb9e492
Maksym <qlowlight@gmail.com>
2023-03-31 13:07:51 +0300

    add jest config, update git ignore

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testTimeout: 100000, //thrown: "Exceeded timeout of 5000 ms for a test."
+
+}
