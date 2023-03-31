95c7c216939acf2bc8baac9630a3299c1b28c086
Maksym <qlowlight@gmail.com>
2023-03-31 13:05:53 +0300

    add prams id validator

--- src/middlewares/validators/params-id-validator.ts
+++ src/middlewares/validators/params-id-validator.ts
@@ -1,3 +1,14 @@
+import {ObjectId} from "mongodb";
+import {NextFunction,Request, Response} from "express";
 
 
 
+
+export const validateId = (req:Request,res:Response,next:NextFunction) => {
+
+    if(!ObjectId.isValid(req.params.id)) {
+            return res.sendStatus(404)
+
+    }
+    return next();
+}
