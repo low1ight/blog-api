8a947f9dab934080080a3736222c954bd5f738d7
Maksym <qlowlight@gmail.com>
2023-04-01 10:47:47 +0300

    add basic CRUD e2e tests for posts

--- __tests__/e2e/posts.e2e.test.ts
+++ __tests__/e2e/posts.e2e.test.ts
@@ -5,9 +5,24 @@ import {app} from "../../src/app";
 
 describe('base posts tests', () => {
 
-
+    let blogId: string
+    let postId: string
+    let blogName: string
 
     beforeAll(async () => {
+
+        const result = await request(app)
+            .post('/api/blogs')
+            .send({
+                name: "UserName",
+                description: "UserDescription",
+                websiteUrl: "https://www.li11111psum.com/"
+            })
+
+        expect(result.status).toEqual(201)
+
+        blogId = result.body.id
+        blogName = result.body.name
 
 
         await runDb()
@@ -18,7 +33,7 @@ describe('base posts tests', () => {
     })
 
 
-    it('should return a 200 status code and an empty arr',  async () => {
+    it('should return a 200 status code and an empty arr', async () => {
         const result = await request(app).get('/api/posts')
 
         expect(result.status).toEqual(200)
@@ -27,10 +42,117 @@ describe('base posts tests', () => {
     })
 
 
+    it('should return a 400 if post created for now-existed blog', async () => {
+        const result = await request(app).post('/api/posts').send({
+            title: "New post",
+            shortDescription: "new post desciption",
+            content: "new post content",
+            blogId: "64072288a950830f9f23d79a"
+        })
+
+        expect(result.status).toEqual(400)
+
+    })
+
+    it('shold successful create a post with 201 status', async () => {
+        const result = await request(app).post('/api/posts').send({
+            title: "New post",
+            shortDescription: "new post desciption",
+            content: "new post content",
+            blogId: "64072288a950830f9f23d79a"
+        })
+
+        expect(result.status).toEqual(201)
+
+
+        postId = result.body.id
+
+        expect(result.body).toEqual(expect.objectContaining({
+            id: expect.any(String),
+            title: "New post",
+            shortDescription: "new post desciption",
+            content: "new post content",
+            blogId: blogId,
+            blogName: blogName
+
+        }))
+    })
+
+    it('should return a 404 status', async () => {
+        const result = await request(app).put('/api/posts/' + 1).send({
+            title: "Updated post",
+            shortDescription: "Updated desciption",
+            content: "Updated content",
+            blogId: blogId
+        })
+
+        expect(result.status).toEqual(404)
+
+
+    })
 
 
 
 
+    it('should successful create a post with 201 status', async () => {
+        const result = await request(app).put('/api/posts/' + postId).send({
+            title: "Updated post",
+            shortDescription: "Updated desciption",
+            content: "Updated content",
+            blogId: blogId
+        })
+
+        expect(result.status).toEqual(204)
+
+
+    })
+
+    it('should return a 404', async () => {
+        const result = await request(app).get('/api/posts/' + 1)
+
+        expect(result.status).toEqual(404)
+
+
+    })
+
+    it('should return a 200 status code and correct post', async () => {
+        const result = await request(app).get('/api/posts/' + postId)
+
+        expect(result.status).toEqual(200)
+
+        expect(result.body).toEqual(expect.objectContaining({
+            id: expect.any(String),
+            title: "Updated post",
+            shortDescription: "Updated desciption",
+            content: "Updated content",
+            blogId: blogId,
+            blogName: blogName
+        }))
+    })
+
+    it('should return a 404', async () => {
+        const result = await request(app).delete('/api/posts/' + 1)
+
+        expect(result.status).toEqual(404)
+
+
+    })
+
+    it('should return a 200 status code and correct post', async () => {
+        const result = await request(app).delete('/api/posts/' + postId)
+
+        expect(result.status).toEqual(204)
+
+    })
+
+
+    it('should return a 200 status code and an empty arr', async () => {
+        const result = await request(app).get('/api/posts')
+
+        expect(result.status).toEqual(200)
+
+        expect(result.body).toEqual([])
+    })
 
 
 
