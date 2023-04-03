245fab503c3765c42f22cdf041b2f791a3d8f0c5
Maksym <qlowlight@gmail.com>
2023-04-03 10:26:59 +0300

    add post-for-blog validation

---
+++ src/middlewares/validators/post/post-for-blog-validation.ts
@@ -0,0 +1,33 @@
+import {body, param} from "express-validator";
+import {errMessages} from "../../../utils/errors/errMessages";
+import {isBlogExist} from "../_custom/isBlogExist";
+
+
+const allFields = ["title","shortDescription","content"]
+
+const [title,shortDescription,content] = allFields;
+
+
+
+
+export const postForBlogValidator = [
+
+    body(allFields)
+        .exists().withMessage(errMessages.notExist())
+        .trim().notEmpty().withMessage(errMessages.isEmpty())
+        .isString().withMessage(errMessages.incorrectType('String')),
+
+
+    body(title)
+        .isLength({max:30}).withMessage(errMessages.tooLong(30)),
+
+
+    body(shortDescription)
+        .isLength({max:100}).withMessage(errMessages.tooLong(100)),
+
+
+    body(content)
+        .isLength({max:1000}).withMessage(errMessages.tooLong(1000)),
+
+
+]
