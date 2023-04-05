1fc18ae9fb42b3d93902bb2d17d1f87bd1ab74b9
Maksym <qlowlight@gmail.com>
2023-04-05 11:30:44 +0300

    add userQueryMapper

---
+++ src/routes/query-mappers/user-query-mapper.ts
@@ -0,0 +1,23 @@
+import {UserInputQueryType} from "../../types/queryType/user/user-input-query-type";
+import {UserQueryType} from "../../types/queryType/user/user-query-type";
+
+
+
+export const blogQueryMapper = ({searchLoginTerm,searchEmailTerm,sortBy,sortDirection,pageNumber,pageSize}:UserInputQueryType):UserQueryType => {
+    return {
+        searchLoginTerm: searchLoginTerm || null,
+        searchEmailTerm:searchEmailTerm || null,
+        sortBy: sortBy || "createdAt",
+        sortDirection: sortDirection || "desc",
+        pageNumber:Number(pageNumber) || 1,
+        pageSize: Number(pageSize) || 10
+
+    }
+}
+
+
+
+
+
+
+
