91c6b391d1064ccdcc04a8e4dcebe5b7d9820e0a
Maksym <qlowlight@gmail.com>
2023-04-21 11:46:19 +0300

    add delete all comment (testing repository)

--- src/repository/testing/testing-repository.ts
+++ src/repository/testing/testing-repository.ts
@@ -2,7 +2,7 @@ import {Blog} from "../../db/models/blog";
 import {Post} from "../../db/models/post";
 import {User} from "../../db/models/user";
 import {Device} from "../../db/models/device";
-
+import {Comment} from "../../db/models/comment";
 
 export class TestingRepository  {
 
@@ -12,6 +12,7 @@ export class TestingRepository  {
         await Post.deleteMany()
         await User.deleteMany()
         await Device.deleteMany()
+        await Comment.deleteMany()
 
 
     }
