b0ef120a2cf7bd1e30f3fd66ad263c0468132200
Maksym <qlowlight@gmail.com>
2023-04-21 11:47:15 +0300

    edit db user and comment schema

--- src/db/schemas/comment-schema.ts
+++ src/db/schemas/comment-schema.ts
@@ -1,18 +1,30 @@
 import {Schema} from "mongoose";
 import {CommentatorInfo, CommentDBType} from "../../types/models/comment/comment-DB-type";
 import {ObjectId} from "mongodb";
+import {CommentLikeStatus} from "../../types/models/comment/comment-like-status";
 
 export const commentatorSchema = new Schema<CommentatorInfo>({
     userId: {type: ObjectId, required: true},
     userLogin: {type: String, required: true},
 })
-
+export const likeDislikeSchema = new Schema<CommentLikeStatus>({
+    userId: {type: Schema.Types.ObjectId, required: true},
+},{ _id : false ,timestamps: { createdAt: 'createdAt' }})
 
 export const commentSchema = new Schema<CommentDBType>({
     content: {type: String, required: true},
     postId: {type: ObjectId, required: true},
     commentatorInfo: {type: commentatorSchema, required: true},
+    likes: { type: [likeDislikeSchema], default: [] },
+    dislikes: { type: [likeDislikeSchema], default: [] },
 
-}, { timestamps: true })
+
+}, { timestamps: true ,toJSON: { virtuals: true } })
 
 
+commentSchema.virtual('likesCount').get(function() {
+    return this.likes.length;
+});
+commentSchema.virtual('dislikesCount').get(function() {
+    return this.dislikes.length;
+});
--- src/db/schemas/user-schema.ts
+++ src/db/schemas/user-schema.ts
@@ -1,22 +1,50 @@
 import {Schema} from "mongoose";
 
-import {UserConfirmation, UserData, UserDBType} from "../../types/models/user/user-DB-type";
+import {
+    UserConfirmation,
+    UserData,
+    UserDBType,
+    UserCommentLike,
+    ActivityElem
+} from "../../types/models/user/user-DB-type";
+
+
 
 export const userDataSchema = new Schema<UserData>({
     login: {type: String, required: true},
     password: {type: String, required: true},
-    passwordRecoveryCode: {type: String, default:null},
+    passwordRecoveryCode: {type: String, default: null},
     email: {type: String, required: true},
-}, { timestamps: true , _id:false},)
+}, {timestamps: true, _id: false},)
 
 
 export const userConfirmation = new Schema<UserConfirmation>({
     confirmationCode: {type: String, required: true},
     isConfirmed: {type: Boolean, required: true},
     expirationDate: {type: Date, required: true},
-}, {_id:false})
+}, {_id: false})
+
+
+export const userLikeAndDislikeSchema = new Schema<UserCommentLike>({
+    commentId: {type: Schema.Types.ObjectId, required: true},
+}, {_id: false ,timestamps: { createdAt: 'createdAt' }})
+
+
+export const userActivityElemSchema = new Schema<ActivityElem>({
+    likes: {type: [userLikeAndDislikeSchema], default: []},
+    dislikes: {type: [userLikeAndDislikeSchema], default: []}
+}, {_id: false})
 
 export const userSchema = new Schema<UserDBType>({
     userData: {type: userDataSchema, required: true},
     userConfirmation: {type: userConfirmation, required: true},
-}, )
+    userActivity: {
+        commentActivity: {
+            type: userActivityElemSchema,
+            default: {
+                likes: [],
+                dislikes: []
+            }
+        }
+    }
+},)
