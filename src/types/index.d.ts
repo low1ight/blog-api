486f8a79060562866da409894d3f954b14a9156a
Maksym <qlowlight@gmail.com>
2023-04-21 11:45:46 +0300

    add userActivity to req

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,12 @@
 import {AccessTokenPayloadData} from "./models/jwt/AccessTokenPayloadData";
+import {UserActivity} from "./models/user/user-DB-type";
 
 
 declare global {
     declare namespace Express {
         export interface Request {
             authUserData:AccessTokenPayloadData | null
+            userActivity:UserActivity | null
         }
     }
 }
